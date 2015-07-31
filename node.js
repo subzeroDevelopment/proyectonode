3f58dc8830395efc2d36d9d1de38cbc08bd1dfb9
codeneomatrix <josuecevedo@gmail.com>
2015-07-30 22:42:07 -0500

    mongo
    insertando y mostrando informacion en  mongobd

--- node.js
+++ node.js
@@ -1,18 +1,21 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var pg = require('pg');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 //mongo
 mongoose.connect("mongodb://127.0.0.1/comentarios");
 var comentariosesquema ={
-	Title: String,
+	titulo: String,
 	usuario:String,
 	comentario:String,
 	estrellas:Number
 };
-var comentario = mongoose.model("comentario",comentariosesquema);
+var Comentario = mongoose.model("comentario",comentariosesquema);
 //
 //postgres
 var conString = "postgres://postgres:pelicula@127.0.0.1/netflix";
@@ -23,11 +26,10 @@ client.connect();
 app.set("view engine","jade");
 app.use(express.static("public"));
 
+
 app.get("/", function(req, res){
 console.log("cliente conectado");
-
 res.render("index");
-
 });
 
 app.get("/newpelicula",function(req,res){
@@ -40,8 +42,27 @@ app.get("/newcomentario",function(req,res){
 
 
 app.post("/comentario",function(req,res){
-	console.log(req);
-	res.render("crearpelicula");
+	console.log(req.body);
+	var data ={
+		titulo: req.body.titulo,
+		usuario: req.body.usuario,
+		comentario: req.body.comentario,
+		estrellas: req.body.estrellas
+	};
+	var coment = new Comentario(data);
+	coment.save(function(err){
+		console.log(coment);
+		res.redirect("/comentarios");
+	})
+	
+});
+
+
+app.get("/comentarios",function(req,res){
+	Comentario.find(function(err,documento){
+		res.render("comentario",{comentarios: documento});
+	});
+
 });
 
 app.get("/pelicula",function(req,res){
