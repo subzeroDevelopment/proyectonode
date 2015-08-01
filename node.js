2a1b39d8482f76ad2ab4ffbbf42c70d577f752a8
codeneomatrix <josuecevedo@gmail.com>
2015-07-31 22:34:08 -0500

    mongo
    pasando completamente a mongo

--- node.js
+++ node.js
@@ -8,21 +8,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //mongo
-/*mongoose.connect("mongodb://127.0.0.1/comentarios");
-var comentariosesquema ={
-	titulo: String,
-	usuario:String,
-	comentario:String,
-	estrellas:Number
+mongoose.connect("mongodb://127.0.0.1/peliculas");
+var peliculasesquema ={
+	titulocoment: String,
+	usuariocoment:String,
+	pelicomentario:String,
+	peliestrellas:Number
 };
-var Comentario = mongoose.model("comentario",comentariosesquema);
 //
-//postgres
-var conString = "postgres://postgres:pelicula@127.0.0.1/netflix";
-var client = new pg.Client(conString);
-client.connect();         
-//
-*/
+
+var Peliculas = mongoose.model("pelicula",peliculasesquema);
+
 app.set("view engine","jade");
 app.use(express.static("public"));
 
@@ -40,18 +36,17 @@ app.get("/newcomentario",function(req,res){
 	res.render("crearcomentario");
 });
 
-/*
 app.post("/comentario",function(req,res){
 	console.log(req.body);
 	var data ={
-		titulo: req.body.titulo,
-		usuario: req.body.usuario,
-		comentario: req.body.comentario,
-		estrellas: req.body.estrellas
+		titulocoment: req.body.titulo,
+		usuariocoment: req.body.usuario,
+		pelicomentario: req.body.comentario,
+		peliestrellas: req.body.estrellas
 	};
-	var coment = new Comentario(data);
-	coment.save(function(err){
-		console.log(coment);
+	var peli = new Peliculas(data);
+	peli.save(function(err){
+		console.log(peli);
 		res.redirect("/comentarios");
 	})
 	
@@ -59,7 +54,7 @@ app.post("/comentario",function(req,res){
 
 
 app.get("/comentarios",function(req,res){
-	Comentario.find(function(err,documento){
+	Peliculas.find(function(err,documento){
 		res.render("comentario",{comentarios: documento});
 	});
 
@@ -67,20 +62,8 @@ app.get("/comentarios",function(req,res){
 
 app.get("/pelicula",function(req,res){
 
-	client.query('SELECT * FROM netflix.pelicula', function(err, result) {
-    
-    console.log(result.rows[0]);
-
-	res.render("pelicula", { peliculas: result.rows[0]});
-    
-    if(err) {return console.error('error running query', err);}
-
-
-  });
-
-
 });
-*/
+
 
 app.listen(8080);
 console.log("servidor iniciado!!!");
